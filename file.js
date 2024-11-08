a487deb8aba9d8dd28298a542084e3f3e47ae113
omscsamilcup <112487226+omscsamilcup@users.noreply.github.com>
2024-11-08 10:58:38 +0800

    Update file.js(

--- file.js
+++ file.js
@@ -8,10 +8,4 @@ ll.registerPlugin(
     /* otherInformation */ null
 ); 
 
-var money = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/Money')
-var rank = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/rank')
-var data = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/data')
-var rebirth = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/rebirth')
-var play_time = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/play_time')
-var cave_level = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/cave_level')
-var mine_level = new KVDatabase('C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/miner_level')
+var db = newKVDatabase(C:/DTSV_Server/DTSV_Lite_Server/plugins/AllConfig_Data/data)
